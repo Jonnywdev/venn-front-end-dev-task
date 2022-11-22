eafc120633a3e18126a2f08f459a76f4e079a1b1
Jonnywdev <jonnywhitt19@gmail.com>
2022-11-22 12:45:49 +0000

    Add all extra css and js features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-// Get the button, and when the user clicks on it, execute myFunction
 document.getElementById("dropdownActivator").onclick = function() {dropdownList()};
+let totopbtn = document.getElementById("toTop");
 
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+window.onscroll = function() {scrollUpFunction()};
+
 function dropdownList() {
     document.getElementById("phoneDropdown").classList.toggle("show");
+}
+
+function scrollUpFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        totopbtn.style.display = "block";
+    } else {
+        totopbtn.style.display = "none";
+    }
+}
+
+function backToTop() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
